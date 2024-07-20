885e264298ea03815bbc7505b6f510adfc647bd4
Adam Ralph <adam@adamralph.com>
2024-07-20 18:19:40 +0200

    MSBuild caching

---
+++ MSBuild.Caching/CacheSet.cs
@@ -0,0 +1,19 @@
+using Microsoft.Build.Framework;
+using Task = Microsoft.Build.Utilities.Task;
+
+namespace MSBuild.Caching;
+
+public class CacheSet : Task
+{
+    public string? Key { get; set; }
+
+    public string? Value { get; set; }
+
+    public override bool Execute()
+    {
+        this.BuildEngine4.RegisterTaskObject(
+            this.Key, this.Value, RegisteredTaskObjectLifetime.Build, allowEarlyCollection: false);
+
+        return true;
+    }
+}
